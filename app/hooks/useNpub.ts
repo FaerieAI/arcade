b12e3859fc44bfdcfe87c54f94a808dfef84987e
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-07-11 12:17:34 -0400

    Updated app/hooks/useNpub.ts

--- app/hooks/useNpub.ts
+++ app/hooks/useNpub.ts
@@ -4,5 +4,28 @@ import { nip19 } from "nostr-tools"
 export function useNpub() {
   const { userStore } = useStores()
   const npub = nip19.npubEncode(userStore.pubkey)
-  return npub
-}
+
+  // Add copy button to copy the npub code  
+  const [copied, setCopied] = React.useState(false);
+
+  return (  
+    <>  
+      <Text style={styles.text}>{npub}</Text>  
+
+      <TouchableOpacity onPress={() => {  
+        Clipboard.setString(npub);  
+        setCopied(true);  
+
+        setTimeout(() => {  
+          setCopied(false);  
+        }, 2000);  
+
+      }}>  
+
+        <Text style={styles.copyButton}>{copied ? 'Copied' : 'Copy'}</Text>   
+
+      </TouchableOpacity>   
+
+    </>   
+
+  )    														       }
